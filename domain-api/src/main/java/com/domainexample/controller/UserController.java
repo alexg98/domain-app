f435e5481103aece602490224ac6f9bb5c760ea0
Alexander Guerrero <alexg98@gmail.com>
2018-01-01 11:58:56 -0500

    clean project

--- domain-api/src/main/java/com/domainexample/controller/UserController.java
+++ domain-api/src/main/java/com/domainexample/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.domainexample.controller;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -55,6 +60,26 @@ public class UserController {
 		this.userService.deleteUser(user);
 		
 	}
+	
+	@RequestMapping(value ="/public", method = RequestMethod.GET)
+	public String getPublic() {
+		return "Acceso metodo publico";
+	} 
+	
+	@RequestMapping(value ="/private", method = RequestMethod.GET)
+	public String getPrivate() {
+		return "Acceso metodo privado";
+	} 
+	
+	@GetMapping
+	public String Hello(@AuthenticationPrincipal final UserDetails userDetails) {
+		String user = userDetails.getUsername();
+		Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+		authorities.stream().
+			forEach(System.out::println);
+		return "Hello world";
+	} 
+	
 	
 	private boolean validate(User user) {
 		boolean isValid = true;
